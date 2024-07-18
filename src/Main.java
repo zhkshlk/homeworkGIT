1a8efc594476b47dfbd523674bd30740d4b0fbbd
Zhakshylyk <zhakshylykk@gmail.com>
2024-07-18 16:12:08 +0600

    мое первое изменение в ветке

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("моя домашняя работа по Git");
+        System.out.println("мое первое изменения для ветки jackie от main");
     }
 }
