d4cfb36f9e60f07ab91f0128a0af98ad3342d8cb
Zhakshylyk <zhakshylykk@gmail.com>
2024-07-19 10:42:44 +0600

    клонированный файл с гита

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("моя домашняя работа по Git");
+        System.out.println("это клонированный файл с гита");
     }
 }
